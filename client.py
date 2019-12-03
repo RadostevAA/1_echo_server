ea2652e9ddd7692b5e1719090671b9b0b2eafb93
RadostevAA <55791524+RadostevAA@users.noreply.github.com>
2019-12-03 18:06:36 +0300

    Выполнение 4 задания
    Выполнено

--- client.py
+++ client.py
@@ -3,14 +3,18 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
+
 sock.connect(('localhost', 9090))
+print('Соединение с сервером.')
 
 msg = input()
+print('Отправка данных серверу.')
 #msg = "Hi!"
 sock.send(msg.encode())
 
+print('Прием данных от сервера.')
 data = sock.recv(1024)
-
+print('Разрыв соединения с сервером.')
 sock.close()
 
 print(data.decode())
